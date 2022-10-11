030da7ed85bda0e68d6870a7bae3d143f116107f
Parastou Safajou <pr2.leclair@gmail.com>
2022-10-10 22:13:04 -0700

    Created user-name Component

---
+++ apps/hacktoberfest2022/src/app/components/user-name/user-name.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hacktoberfest2022-user-name',
+  templateUrl: './user-name.component.html',
+  styleUrls: ['./user-name.component.scss'],
+})
+export class UserNameComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
