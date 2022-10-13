ab5a3553e95f3af2658a4eeda9c390ae93598ba2
mallenswe <45367468+mallenswe@users.noreply.github.com>
2022-10-13 00:18:42 -0700

    Email-List: Added Email List Component

--- apps/hacktoberfest2022/src/app/app.module.ts
+++ apps/hacktoberfest2022/src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserNameComponent } from './components/user-name/user-name.component';
+import { EmailListComponent } from './components/email-list/email-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, UserNameComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    UserNameComponent,
+    EmailListComponent,
+  ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
