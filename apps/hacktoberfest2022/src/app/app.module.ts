3bac696e7138e200f7d0c3fd05727fe4299ae3ac
mallenswe <45367468+mallenswe@users.noreply.github.com>
2022-10-13 00:17:34 -0700

    Email-Row: Added Email Row Component

--- apps/hacktoberfest2022/src/app/app.module.ts
+++ apps/hacktoberfest2022/src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserNameComponent } from './components/user-name/user-name.component';
+import { EmailRowComponent } from './components/email-row/email-row.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, UserNameComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    UserNameComponent,
+    EmailRowComponent,
+  ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
